2c530387570c974c5621817bf978bd1e892ef913
nickmazurenko <surprisedsunrise@gmail.com>
2022-08-04 15:23:23 +0300

    add circleArray class

---
+++ Task_3/circleArray.js
@@ -0,0 +1,29 @@
+class circleArray {
+  constructor(...args) {
+    this.arr = [];
+    for (let idx in args) this.add(args[idx]);
+  }
+
+  add(data) {
+    this.arr.push(data);
+  }
+
+  get(index) {
+    let getIndex = (index, length) => {
+      while (index > length - 1) index -= length;
+      return index;
+    };
+
+    if (index > this.arr.length) {
+      return this.arr[getIndex(index, this.arr.length)];
+    }
+
+    if (index < 0) {
+      return this.arr[
+        getIndex(Math.abs(this.arr.length + index), this.arr.length)
+      ];
+    }
+  }
+}
+
+export default circleArray;
